e5df3eaa92f5dca32aefb64033cbb405311a1b77
exief <cookster455@hotmail.com>
2017-10-24 10:09:58 +1030

    Create Server class declaration

---
+++ server.h
@@ -0,0 +1,26 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <string>
+#include <arpa/inet.h>
+
+class Server {
+public:
+	Server();
+	Server(const int& port);
+	Server(const std::string& ip, const int& port);
+	Server(const Server& other);
+	
+	Server& operator=(const Server& rhs);
+	
+	bool operator==(const Server& rhs) const;
+	bool operator!=(const Server& rhs) const;
+	
+	void send(const std::string& msg);
+	void receive();
+protected:
+private:
+	struct sockaddr_in socketAddress_;
+	int fd_;
+};
+#endif
