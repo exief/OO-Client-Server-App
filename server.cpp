9cb9cbd4046257488bb13373930b1a321f8122d6
exief <cookster455@hotmail.com>
2017-10-24 15:13:32 +1030

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,47 +1,58 @@
 #include "server.h"
 
-Server::Server() {
+Server::Server(const int& port = 5000) {
 	this->fd_ = socket(AF_INET, SOCK_STREAM, 0);
 	
 	this->socketAddress_.sin_family = AF_INET;
 	this->socketAddress_.sin_addr.s_addr = htonl(INADDR_ANY);
-	this->socketAddress_.sin_port = htons(5000);
+	this->socketAddress_.sin_port = htons(port);	
 	
 	bind(this->fd_, (struct sockaddr*) &this->socketAddress_, sizeof(this->socketAddress_));
-	
-	listen(this->fd_, MAX_CONNECTIONS);
-}
-
-Server::Server(const int& port) {
-
-}
-
-Server::Server(const std::string& ip, const int& port) {
-
 }
 
 Server::Server(const Server& other) {
-
+	*this = other;
 }
 
 Server::~Server() {
-
+	
 }
 	
 Server::Server& operator=(const Server& rhs) {
-
+	this->fd_ = socket(AF_INET, SOCK_STREAM, 0);
+	
+	this->socketAddress_.sin_family = AF_INET;
+	this->socketAddress_.sin_addr.s_addr = htonl(INADDR_ANY);
+	this->socketAddress_.sin_port = htons(rhs.socketAddress_.sin_port);	
+	
+	bind(this->fd_, (struct sockaddr*) &this->socketAddress_, sizeof(this->socketAddress_));
 }
 	
 Server::bool operator==(const Server& rhs) const {
-
+	if(this->socketAddress_.sin_family != rhs.socketAddress_.sin_family) {
+		return false;	
+	} else if (this->socketAddress_.sin_addr.s_addr != rhs.socketAddress_.sin_addr.s_addr) {
+		return false;
+	} else if (this->socketAddress_.sin_port != rhs.socketAddress_.sin_port) {
+		return false;
+	}
+	return true;
 }
 
 Server::bool operator!=(const Server& rhs) const { 
-
+	return !(*this == rhs);
 }
 	
-void Server::send(const std::string& msg) const {
+void Server::listen() {
+	listen(this->fd_, MAX_CONNECTIONS);
+}
 
+void Server::send(const std::string& msg) const {
+	try {
+		if(send(this->fd_, msg.c_str(), msg.length(), 0) < 0) throw -1;
+	} catch (int e) {
+		std::cout << "Error Sending Message" << std::endl;
+	}
 }
 
 void Server::receive() {
