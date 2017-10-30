ea2530b68ff1edbef9de1ccdc544efc6d3d1db5b
exief <cookster455@hotmail.com>
2017-10-30 15:39:02 +1030

    Update server-main.cpp

--- server-main.cpp
+++ server-main.cpp
@@ -8,10 +8,12 @@
 int main(int argc, char** argv){
 	try {
 		Server* svr = new Server();
+		svr->listen();
+		svr->accept();
+		svr->~Server();
 	} catch(...) {
 		std::cerr << "Error Creating Server Object" << std::endl;
 		return EXIT_FAILURE;
 	}
-
 	return EXIT_SUCCESS;
 }
